6d5b5b1cac616483feefa6c0cf48e1127cb2c17a
RoseCherian-cargoz <rose@cargoz.com>
2025-07-26 09:59:47 +0400

    dg highlight

--- lead_form.py
+++ lead_form.py
@@ -106,7 +106,7 @@ if commodity_type == "DG":
         "Upload MSDS Document", type=["pdf", "docx", "jpg", "png"], key="msds_uploader"
     )
 with row2_col2:
-    commodity = st.text_input("Commodity", key="commodity_text_input")
+    # commodity = st.text_input("Commodity", key="commodity_text_input")
 
 # Row 3: Storage Type (Left) | Required Temperature (Right if needed)
 row3_col1, row3_col2 = st.columns(2)
