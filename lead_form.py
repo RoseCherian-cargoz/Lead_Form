a293a3fcce89e296cfd0abdfcec44e4ba07a2a9e
RoseCherian-cargoz <rose@cargoz.com>
2025-08-12 12:54:28 +0400

    check

--- lead_form.py
+++ lead_form.py
@@ -759,7 +759,7 @@ if st.button("Submit Form"):
         "Avg Weight (KG)": average_weight if 'average_weight' in locals() else "N/A",
         "Dimensions (L X W X H in cm)": dimensions if 'dimensions' in locals() else "N/A",
         "Approximate Space Required": approx_space if 'approx_space' in locals() else "N/A",
-        "Space Unit": space_unit,
+        "Space Unit": detailed_space_unit,
         "Packing List File (link)": packing_list_link,
         "Handling In Required [Yes/No]": handling_in,
         "Handling Out Required [Yes/No]": handling_out,
